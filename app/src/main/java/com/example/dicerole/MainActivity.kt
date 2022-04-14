16a1159856c22041303f515882c82c729e747f81
nugrohoaziz <azizagusn17@gmail.com>
2022-04-14 19:34:32 +0700

    dellet lucky number

--- app/src/main/java/com/example/dicerole/MainActivity.kt
+++ app/src/main/java/com/example/dicerole/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.dicerole
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -30,11 +29,6 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             else -> R.drawable.dice_5
-        }
-        if (diceRoll == luckyNumber){
-             Toast.makeText(this, "yeah you win", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "try again roll dice!", Toast.LENGTH_SHORT).show()
         }
         diceImage.setImageResource(drawableResource)
         diceImage.contentDescription = diceRoll.toString()
