781ce4ab1c49b152530356221a947269b1d79d4b
nugrohoaziz <azizagusn17@gmail.com>
2022-04-15 10:52:47 +0700

    add 2 dice with the lucky dice Add a button to an app (practice 4)

--- app/src/main/java/com/example/dicerole/MainActivity.kt
+++ app/src/main/java/com/example/dicerole/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerole
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,18 +13,27 @@ class MainActivity : AppCompatActivity() {
 
         val buttonRoll: Button = findViewById(R.id.buttonRoll)
         buttonRoll.setOnClickListener {
-            diceRoll()
+            diceRoll1()
         }
     }
 
-
-    private fun diceRoll() {
+    private fun diceRoll1() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val luckyNumber = 6
+        val dice2 = Dice2(6)
+        val diceRolling = dice.roll()
+        val diceRolling2 = dice2.roll()
         val diceImage: ImageView = findViewById(R.id.imageview1)
-        val drawableResource = when(diceRoll){
-            luckyNumber -> R.drawable.dice_6
+        val diceImage2: ImageView = findViewById(R.id.imageview2)
+        val drawableResource = when(diceRolling){
+            6 -> R.drawable.dice_6
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            else -> R.drawable.dice_5
+        }
+        val drawableResource2 = when(diceRolling2){
+            6 -> R.drawable.dice_6
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,11 +41,21 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_5
         }
         diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
-
+        diceImage2.setImageResource(drawableResource2)
+        if (drawableResource == drawableResource2){
+            Toast.makeText(this,"yeah you win", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "please try again", Toast.LENGTH_SHORT).show()
+        }
+        diceImage.contentDescription = diceRolling.toString()
     }
 
     class Dice(var numSide: Int){
+        fun roll (): Int{
+            return (1..numSide).random()
+        }
+    }
+    class Dice2(var numSide: Int){
         fun roll (): Int{
             return (1..numSide).random()
         }
